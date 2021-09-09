245dbebe62224269de9871868a5d4c3d376f8ab2
Stephanie Alyssa <alyssa.yesilyurt@gmail.com>
2021-09-09 13:46:39 -0400

    create with spring initializer

---
+++ bookshopartifact/src/test/java/bookshopapp/bookshopartifact/BookshopartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package bookshopapp.bookshopartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
