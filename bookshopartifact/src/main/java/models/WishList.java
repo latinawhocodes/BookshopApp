902c39491ac1e50856a4281d6b5023737c646bde
Stephanie Alyssa <alyssa.yesilyurt@gmail.com>
2021-11-15 18:23:46 -0500

    Revert "Merge branch 'origin' of https://github.com/latinawhocodes/BookshopApp into origin"
    This reverts commit 7ff598d4363112c1890ddafe0c831931da19d15f, reversing
    changes made to 7ce16d8632855bd98f5d04f83f51ab2711487c9a.

--- bookshopartifact/src/main/java/models/WishList.java
+++ bookshopartifact/src/main/java/models/WishList.java
@@ -1,7 +1,5 @@
 package models;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -11,56 +9,24 @@ import java.util.List;
 public class WishList {
 	private String WishListName;
 	private String Username;
-    //private String BookName;
     private List <Book> books;
 
-	private String isbn;
+//	private String ISBN;
 //	private String BookName;
 //	private String BookAuthor;
 	
 	public WishList () {
 		
 	}
-
-    public WishList (String wishlistName, String username) {
-        //System.out.println("used constructor 1");
-		this.WishListName = wishlistName;
-		this.Username = username;
-        //this.BookName = bookName;
-        books = new ArrayList <Book>();
-        this.isbn = null;
-		//this.ISBN = ISBN;
-		//this.BookName = BookName;
-		//this.BookAuthor = BookAuthor;
-	}
 	
 	//public WishList (String WishListName, String Username, String ISBN, double BookName, String BookAuthor) {
     public WishList (String WishListName, String Username, List<Book> book) {
-       // System.out.println("used constructor 2");
 		this.WishListName = WishListName;
 		this.Username = Username;
         this.books = book;
-        this.isbn = null;
-        //this.BookName = BookName;
 		//this.ISBN = ISBN;
 		//this.BookName = BookName;
 		//this.BookAuthor = BookAuthor;
-	}
-
-    public WishList (String wishlistName, String username, String isbn) {
-       // System.out.println("used constructor 3");
-		this.WishListName = wishlistName;
-		this.Username = username;
-        this.isbn = isbn;
-       // this.books = book;
-       List <Book> books = Book.GetAllBooks();
-	   List <Book> bookToAdd = new ArrayList <Book> ();
-		    for (Book b : books) {
-			    if (b.getISBN ().equals(isbn)) {
-				    bookToAdd.add(b);			
-                    this.books = bookToAdd;
-			    }
-            }
 	}
 
     public void addBook(Book b) {
@@ -77,19 +43,6 @@ public class WishList {
 
 	public void setWishListName(String wishListName) {
 		WishListName = wishListName;
-	}
-
-    public String getisbn() {
-		return isbn;
-	}
-
-
-    /*public String getBookName() {
-		return BookName;
-	}*/
-
-	public void setBookISBN(String isbn) {
-		this.isbn = isbn;
 	}
 
 	public String getUsername() {
@@ -122,72 +75,6 @@ public class WishList {
     }
 */
 
-    public WishList createWishlist(WishList wlist) throws SQLException {
-        //WishList wl = new WishList(wishlist, username);
-       // Database db = new Database();
-       // db.SetConnection();
-       Database.SetConnection();
-       Connection con = Database.connection;
-
-        String sql = "INSERT INTO WishLists VALUES (?, ?, ?)";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, wlist.getWishListName());
-        pstmt.setString(2, wlist.getUsername());
-        pstmt.setString(3, null);
-        System.out.println(wlist.getWishListName());
-        System.out.println(wlist.getUsername());       
-        System.out.println(pstmt);
-
-        pstmt.executeUpdate();
-        pstmt.close();
-        return wlist;
-    }
-
-    public WishList addBooktoWishlist(WishList wlist) throws SQLException {
-        //WishList wl = new WishList(wishlist, username);
-       // Database db = new Database();
-       // db.SetConnection();
-       Database.SetConnection();
-       Connection con = Database.connection;
-
-        String sql = "INSERT INTO WishLists VALUES (?, ?, ?)";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, wlist.getWishListName());
-        pstmt.setString(2, wlist.getUsername());
-        pstmt.setString(3, wlist.getisbn());
-        System.out.println(wlist.getWishListName());
-        System.out.println(wlist.getUsername());  
-        System.out.println(wlist.getisbn());     
-        System.out.println(pstmt);
-
-        pstmt.executeUpdate();
-        pstmt.close();
-        return wlist;
-    }
-
-    public WishList removeBookFromWL(WishList wlist) throws SQLException {
-        //WishList wl = new WishList(wishlist, username);
-       // Database db = new Database();
-       // db.SetConnection();
-       Database.SetConnection();
-       Connection con = Database.connection;
-
-        String sql = "DELETE FROM WishLists WHERE WishListName = ? AND Username = ? and ISBN = ?";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, wlist.getWishListName());
-        pstmt.setString(2, wlist.getUsername());
-        pstmt.setString(3, wlist.getisbn());
-        System.out.println(wlist.getWishListName());
-        System.out.println(wlist.getUsername());  
-        System.out.println(wlist.getisbn());     
-        System.out.println(pstmt);
-
-        pstmt.executeUpdate();
-        pstmt.close();
-        return wlist;
-    }
-
-
     public static ArrayList<WishList> GetAllWishLists() {
         ArrayList<WishList> wishList = new ArrayList<WishList>();
         Database.SetConnection();
@@ -201,7 +88,6 @@ public class WishList {
                 String wln = resultSet.getString("WishListName");
                 String un = resultSet.getString("Username");
                 String isbn = resultSet.getString("ISBN");
-                //String name = resultSet.getString("BookName");
                 int i = 0;
 
                 //if there is already a wishlist for that username & wishlist name, just add book to it , otherwise create new wishlist
